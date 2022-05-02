0ae718c3475acdd29a6aa96fb2792bab60be0bb6
david <simplesam8856@gmail.com>
2022-05-03 09:36:28 +1000

    .

---
+++ utils/scroll-observer.tsx
@@ -0,0 +1,33 @@
+import { warn } from 'console'
+import React, { ReactNode, useCallback, useEffect, useState } from 'react'
+
+interface ScrollValue {
+  scrollY: number
+}
+
+export const ScrollContext = React.createContext<ScrollValue>({
+  scrollY: 0
+})
+
+interface Props { // Note the children props require for react 18
+  children?: ReactNode
+}
+
+const ScrollObserver: React.FC<Props> = ({ children }) => {
+  const [scrollY, setScrollY] = useState(0)
+  const handleScroll = useCallback(() => {
+    setScrollY(window.scrollY)
+  }, [])
+
+  useEffect(() => {
+    document.addEventListener('scroll', handleScroll, { passive: true })
+    return () => document.removeEventListener('scroll', handleScroll)
+  }, [handleScroll])
+  return (
+    <ScrollContext.Provider value={{ scrollY }}>
+      {children}
+    </ScrollContext.Provider>
+  )
+}
+
+export default ScrollObserver
